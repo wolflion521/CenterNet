ace99b6f99b8828c793727ae9fe372d64d79ae5e
wolflion521 <15998815467@126.com>
2019-09-14 14:34:02 +0800

    pool functions are used to recore the largest value from the beginning cell to current cell

--- wolf/mypool.cpp
+++ wolf/mypool.cpp
@@ -1,11 +1,31 @@
 #include <torch/torch.h>
-
+#include <iostream>
 #include <vector>
 
 std::vector<at::Tensor> mypool_forward(
     at::Tensor input
 ) {
+    // Initialize output
     at::Tensor output = at::zeros_like(input);
+    std::cout<<output<<std::endl;
+
+    // Get width
+    int64_t width = input.size(3);
+
+    // Copy the last column
+    at::Tensor input_temp  = input.select(3, 0);
+    at::Tensor output_temp = output.select(3, 0);
+    output_temp.copy_(input_temp);
+
+    at::Tensor max_temp;
+    for (int64_t ind = 0; ind < width - 1; ++ind) {
+        input_temp  = input.select(3, ind + 1);
+        output_temp = output.select(3, ind);
+        max_temp    = output.select(3, ind + 1);
+
+        at::max_out(max_temp, input_temp, output_temp);
+    }
+
     return {
         output
     };
@@ -15,7 +35,40 @@ std::vector<at::Tensor> mypool_backward(
     at::Tensor input,
     at::Tensor grad_output
 ) {
-    auto output = at::zeros_like(input);
+     at::Tensor output = at::zeros_like(input);
+
+    int32_t batch   = input.size(0);
+    int32_t channel = input.size(1);
+    int32_t height  = input.size(2);
+    int32_t width   = input.size(3);
+
+    auto max_val = at::zeros(torch::CUDA(at::kFloat), {batch, channel, height});
+    auto max_ind = at::zeros(torch::CUDA(at::kLong),  {batch, channel, height});
+
+    auto input_temp = input.select(3, 0);
+    max_val.copy_(input_temp);
+
+    max_ind.fill_(0);
+
+    auto output_temp      = output.select(3, 0);
+    auto grad_output_temp = grad_output.select(3, 0);
+    output_temp.copy_(grad_output_temp);
+
+    auto un_max_ind = max_ind.unsqueeze(3);
+    auto gt_mask    = at::zeros(torch::CUDA(at::kByte),  {batch, channel, height});
+    auto max_temp   = at::zeros(torch::CUDA(at::kFloat), {batch, channel, height});
+    for (int32_t ind = 0; ind < width - 1; ++ind) {
+        input_temp = input.select(3, ind + 1);
+        at::gt_out(gt_mask, input_temp, max_val);
+
+        at::masked_select_out(max_temp, input_temp, gt_mask);
+        max_val.masked_scatter_(gt_mask, max_temp);
+        max_ind.masked_fill_(gt_mask, ind + 1);
+
+        grad_output_temp = grad_output.select(3, ind + 1).unsqueeze(3);
+        output.scatter_add_(3, un_max_ind, grad_output_temp);
+    }
+
     return {
         output
     };
--- wolf/mytest.py
+++ wolf/mytest.py
@@ -2,18 +2,35 @@ from models.py_utils._cpools import top_pool, bottom_pool,left_pool,right_pool
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import mypool
 
-input = torch.randn(1,10,10)
-print("input",input)
+def test_1():
+    # test what is the input and output of top_pool.cpp
+    input = torch.randn(1,2,10,10)
+    print("input",input)
+    output = right_pool.forward(input)[0]
+    print("output",output)
+    print("output shape: ", output.shape)
+    fig = plt.figure(figsize=(8,8))
+    ax1 = fig.add_subplot(1,2,1)
+    ax1.imshow(input[0][0])
+    ax2 = fig.add_subplot(1,2,2)
+    ax2.imshow(output[0][0])
+    plt.show()
 
+def test_2():
+    # test what is the input and output of top_pool.cpp
+    input = torch.randn(1,1, 5, 5)
+    print("input", input)
+    output = mypool.forward(input)[0]
+    print("output", output)
+    print("output shape: ", output.shape)
+    fig = plt.figure(figsize=(8, 8))
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.imshow(input[0][0])
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.imshow(output[0][0])
+    plt.show()
 
-output = top_pool.forward(input)[0]
-print("output",output)
-print("output shape: ", output.shape)
-
-fig = plt.figure(figsize=(8,8))
-ax1 = fig.add_subplot(1,2,1)
-ax1.imshow(input[0])
-ax2 = fig.add_subplot(1,2,2)
-ax2.imshow(output[0])
-plt.show()
+if __name__ == "__main__":
+    test_1()
